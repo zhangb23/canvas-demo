f3d7fe44a71b798087f559bbc05949a2a09782af
KevinZhang <zhangb23@163.com>
2018-03-06 20:22:43 +0800

    我的画板

---
+++ main.js
@@ -0,0 +1,215 @@
+var yyy = document.getElementById('xxx');
+var context = yyy.getContext('2d');
+var lineWidth = 5
+
+
+autoSetCanvasSize(yyy)
+
+listenToUser(yyy)
+
+
+var eraserEnabled = false
+pen.onclick = function(){
+  eraserEnabled = false
+  pen.classList.add('active')
+  eraser.classList.remove('active')
+}
+eraser.onclick = function(){
+  eraserEnabled = true
+  eraser.classList.add('active')
+  pen.classList.remove('active')
+}
+clear.onclick = function () {
+  context.clearRect(0,0,yyy.width,yyy.height);
+}
+
+save.onclick = function(){
+  var url = yyy.toDataURL("image/png")
+  var a = document.createElement('a')
+  document.body.appendChild(a)
+  a.href = url
+  a.download = '我的画儿'
+  a.target = '_blank'
+  a.click()
+}
+
+black.onclick = function(){
+    context.fillStyle = 'black'
+    context.strokeStyle = 'black'
+    black.classList.add('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+    yellow.classList.remove('active')
+}
+red.onclick = function(){
+  context.fillStyle = 'red'
+  context.strokeStyle = 'red'
+  red.classList.add('active')
+  green.classList.remove('active')
+  blue.classList.remove('active')
+  black.classList.remove('active')
+  yellow.classList.remove('active')
+}
+green.onclick = function(){
+  context.fillStyle = 'green'
+  context.strokeStyle = 'green'
+  red.classList.remove('active')
+  green.classList.add('active')
+  blue.classList.remove('active')
+  black.classList.remove('active')
+  yellow.classList.remove('active')
+}
+blue.onclick = function(){
+  context.fillStyle = 'blue'
+  context.strokeStyle = 'blue'
+  red.classList.remove('active')
+  green.classList.remove('active')
+  blue.classList.add('active')
+  black.classList.remove('active')
+  yellow.classList.remove('active')
+}
+yellow.onclick = function(){
+    context.fillStyle = 'yellow'
+    context.strokeStyle = 'yellow'
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.remove('active')
+    yellow.classList.add('active')
+}
+range.onchange = function(){
+  lineWidth = document.getElementById('range').value
+}
+
+
+/******/
+
+function autoSetCanvasSize(canvas) {
+  setCanvasSize()
+
+  window.onresize = function() {
+    setCanvasSize()
+  }
+
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+
+    canvas.width = pageWidth
+    canvas.height = pageHeight
+  }
+}
+
+function drawCircle(x, y, radius) {
+  context.beginPath()
+  context.arc(x, y, radius, 0, Math.PI * 2);
+  context.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath();
+  context.moveTo(x1, y1) // 起点
+  context.lineWidth = lineWidth
+  context.lineTo(x2, y2) // 终点
+  context.stroke()
+  context.closePath()
+}
+
+function listenToUser(canvas) {
+
+
+  var using = false
+  var lastPoint = {
+    x: undefined,
+    y: undefined
+  }
+  // 特性检测
+  if(document.body.ontouchstart !== undefined){
+    // 触屏设备 
+    canvas.ontouchstart = function(aaa){
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+      using = true
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, lineWidth*2, lineWidth*2)
+      } else {
+        lastPoint = {
+          "x": x,
+          "y": y
+        }
+      }
+    }
+    canvas.ontouchmove = function(aaa){
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+
+      if (!using) {return}
+
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, lineWidth*2, lineWidth*2)
+      } else {
+        var newPoint = {
+          "x": x,
+          "y": y
+        }
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+    canvas.ontouchend = function(){
+      using = false
+    }
+  }else{
+    // 非触屏设备
+    canvas.onmousedown = function(aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
+      using = true
+
+      if (eraserEnabled) {
+        function clearPart(x,y) {
+          //保存场景
+          ctx.save();
+          ctx.beginPath();
+          ctx.arc(x,y,10,0,Math.PI * 2,false);
+          ctx.clip();
+          ctx.clearRect(0,0,canvas.width,canvas.height);
+          
+          //还原场景
+          ctx.restore();
+      }
+        //context.clearRect(x - 5, y - 5, lineWidth*2, lineWidth*2)
+      } else {
+        lastPoint = {
+          "x": x,
+          "y": y
+        }    
+      }
+    }
+    canvas.onmousemove = function(aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
+
+
+      if (!using) {return}
+
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, lineWidth*2, lineWidth*2)
+      } else {
+        var newPoint = {
+          "x": x,
+          "y": y
+        }
+      
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+
+    }
+    canvas.onmouseup = function(aaa) {
+      using = false
+    }
+  }
+
+}
